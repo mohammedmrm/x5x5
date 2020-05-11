57f3b401493904690c32986089a936ae13e93f2c
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-05-11 15:04:48 +0300

    quik add client and store

--- script/_getDriverDetails.php
+++ script/_getDriverDetails.php
@@ -84,8 +84,8 @@ $sql2 = "select driver_invoice.*,date_format(driver_invoice.date,'%Y-%m-%d') as 
            staff.name as driver_name, staff.phone as driver_phone
            from driver_invoice
            left join  staff on staff.id = driver_invoice.driver_id
-           where driver_id=? and driver_invoice.date between ? AND ?";
-$res2 = getData($con,$sql2,[$id,$start,$end]);
+           where driver_id='".$id."' and driver_invoice.date between '".$start."' AND '".$end."'";
+$res2 = getData($con,$sql2);
 if(count($res2) > 0){
     $success = 1;
 }
