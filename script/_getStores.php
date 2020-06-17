9bda91237dff79b1ff6d811417846a5dd68ea526
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-17 23:19:48 +0300

    bugs fix

--- script/_getStores.php
+++ script/_getStores.php
@@ -8,6 +8,11 @@ $client = $_REQUEST['client'];
 
 require("dbconnection.php");
 try{
+  if($_SESSION['role'] == 1 || $_SESSION['role'] == 5 || $_SESSION['role'] == 7){
+       $branch ="";
+  }else{
+   $branch = " and clients.branch_id=".$_SESSION['user_details']['branch_id'];
+  }
   if(empty($client)){
    $query = "select stores.*, clients.name as client_name , clients.phone as client_phone,
              if(date_format(a.old_date,'%Y-%m-%d') is not null,date_format(a.old_date,'%Y-%m-%d'),'9999-12-31') as old_date,a.orders as orders
@@ -19,7 +24,7 @@ try{
                         max(store_id) as store_id
                  from orders where orders.confirm=1
                  group by orders.store_id
-             ) a on a.store_id = stores.id  order by  old_date ASC,orders DESC
+             ) a on a.store_id = stores.id ".$branch." order by  old_date ASC,orders DESC
              ";
     $data = getData($con,$query);
 
@@ -36,7 +41,7 @@ try{
                  group by orders.store_id
              ) a on a.store_id = stores.id
              ";
-   $query .= " where stores.client_id=?  order by  old_date ASC,orders DESC";
+   $query .= " where stores.client_id=? ".$branch." order by  old_date ASC,orders DESC";
    $data = getData($con,$query,[$client]);
   }
   $success="1";
