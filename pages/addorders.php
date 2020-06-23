284b7bff6859e2b5babafc6b8e2cd2e4beeb6068
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-23 11:17:13 +0300

    add orders update

--- pages/addorders.php
+++ pages/addorders.php
@@ -160,7 +160,7 @@ include("config.php");
 			</div>
             <div class="form-group col-lg-2">
             	<label>رقم الوصل:</label>
-            	<input  no="no" id="order_no1" name="order_no[]" barcode="barcode"  type="text" class="form-control kt-input sp" placeholder="">
+            	<input  no="no" id="order_no1" onkeyup="$(this).val($(this).val().replace(/[^0-9]+/g, ''))" name="order_no[]" barcode="barcode"  type="text" class="form-control kt-input sp" placeholder="">
                <span id="order_no_err1" class="form-text text-danger"></span>
            </div>
 			<div class="form-group col-lg-2">
@@ -630,7 +630,7 @@ function addMore(){
 			</div>
             <div class="form-group col-lg-2">
             	<label>رقم الوصل:</label>
-            	<input  no="no" id="order_no`+number+`" value="" name="order_no[]"  type="text" class="form-control sp" placeholder="">
+            	<input  no="no" id="order_no`+number+`" onkeyup="$(this).val($(this).val().replace(/[^0-9]+/g, ''))" value="" name="order_no[]"  type="text" class="form-control sp" placeholder="">
                 <span id="order_no_err`+number+`" class="form-text text-danger"></span>
             </div>
 			<div class="form-group col-lg-2">
@@ -937,72 +937,6 @@ $.ajax({
     console.log(e);
   }
 });
-}
-function CurrencyFormatted(input, blur) {
-  // appends $ to value, validates decimal side
-  // and puts cursor back in right position.
-
-  // get input value
-  var input_val = input.val();
-
-  // don't validate empty input
-  if (input_val === "") { return; }
-
-  // original length
-  var original_len = input_val.length;
-
-  // initial caret position
-  var caret_pos = input.prop("selectionStart");
-
-  // check for decimal
-  if (input_val.indexOf(".") >= 0) {
-
-    // get position of first decimal
-    // this prevents multiple decimals from
-    // being entered
-    var decimal_pos = input_val.indexOf(".");
-
-    // split number by decimal point
-    var left_side = input_val.substring(0, decimal_pos);
-    var right_side = input_val.substring(decimal_pos);
-
-    // add commas to left side of number
-    left_side = formatNumber(left_side);
-
-    // validate right side
-    right_side = formatNumber(right_side);
-
-    // On blur make sure 2 numbers after decimal
-    if (blur === "blur") {
-      right_side += "00";
-    }
-
-    // Limit decimal to only 2 digits
-    right_side = right_side.substring(0, 2);
-
-    // join number by .
-    input_val =  left_side + "." + right_side;
-
-  } else {
-    // no decimal entered
-    // add commas to number
-    // remove all non-digits
-    input_val = formatNumber(input_val);
-    input_val =  input_val;
-
-    // final formatting
-    if (blur === "blur") {
-      input_val += ".00";
-    }
-  }
-
-  // send updated string to input
-  input.val(input_val);
-
-  // put caret back in the right position
-  var updated_len = input_val.length;
-  caret_pos = updated_len - original_len + caret_pos;
-  input[0].setSelectionRange(caret_pos, caret_pos);
 }
 function CurrencyFormatted(input, blur) {
   // appends $ to value, validates decimal side
