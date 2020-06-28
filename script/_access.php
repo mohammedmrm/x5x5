123550b2d69bc0bfb98fc665bbc661880e7b90db
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-28 22:04:28 +0300

    cookie added

--- script/_access.php
+++ script/_access.php
@@ -1,12 +1,12 @@
 <?php
-//error_reporting(0);
+error_reporting(0);
 if(!isset($_SESSION)){
 session_start();
 }
 try{
 
 $con2 = new PDO('mysql:host=localhost;dbname=nahar', "root",
-"Toto1988", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
+"TOto1988", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
 //$con->exec("SET CHARACTER SET UTF8");
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException  $e ){
