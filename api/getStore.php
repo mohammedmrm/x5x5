8345d8551ff08214665085d86c5c867b637beaab
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-15 18:52:50 +0300

    api update

--- api/getStore.php
+++ api/getStore.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-//error_reporting(0);
+error_reporting(0);
 require_once("_apiAccess.php");
-//access();
-$token = "";
+access();
+$token = $_REQUEST['token'];
 require_once("../script/dbconnection.php");
 try{
   $query = "select stores.*, clients.name as client_name , clients.phone as client_phone
@@ -16,5 +16,5 @@ try{
    $data=["error"=>$ex];
    $success="0";
 }
-echo json_encode(["success"=>$success,"data"=>$data,'messgae'=>""]);
+echo json_encode([$query,"success"=>$success,"data"=>$data,'messgae'=>""]);
 ?>
