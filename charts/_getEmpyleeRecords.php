77c68653ab4f2ea1d035c664c46d135dce60c25a
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-08-23 13:55:59 +0300

    some update

--- charts/_getEmpyleeRecords.php
+++ charts/_getEmpyleeRecords.php
@@ -22,12 +22,12 @@ if($_SESSION['user_details']['role_id'] == 1){
 
 $inserted = "select staff.name as name , count(orders.manager_id) as inserted from staff
               left join orders orders on orders.manager_id = staff.id and orders.confirm =1
-              where (date between '".$start."' and '".$end."')
+              where (orders.date between '".$start."' and '".$end."')
               GROUP by staff.id order by count(orders.manager_id) DESC limit 10";
 }else{
 $inserted = "select staff.name as name , count(orders.manager_id) as inserted from staff
               left join orders orders on orders.manager_id = staff.id and orders.confirm =1
-              where (date between '".$start."' and '".$end."') and staff.branch_id = '".$_SESSION['user_details']['branch_id']."'
+              where (orders.date between '".$start."' and '".$end."') and staff.branch_id = '".$_SESSION['user_details']['branch_id']."'
               GROUP by staff.id order by count(orders.manager_id) DESC limit 10";
 }
 $result = getData($con,$inserted);
