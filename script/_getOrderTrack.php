2ebc107389065dcff51164bbd86bc8de66c405a8
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-08-23 13:51:44 +0300

    some update

--- script/_getOrderTrack.php
+++ script/_getOrderTrack.php
@@ -8,11 +8,11 @@ $id= $_REQUEST['id'];
 $success=0;
 try{
   $query = "select tracking.*,order_status.status as status,staff.name as staff_name,
-  DATE_FORMAT(order.date,'%Y-%m-%d') as date,DATE_FORMAT(order.date,'%H:%i') as hour
+  DATE_FORMAT(tracking.date,'%Y-%m-%d') as date,DATE_FORMAT(tracking.date,'%H:%i') as hour
   from tracking
   left join order_status on tracking.order_status_id = order_status.id
   left join staff on tracking.staff_id = staff.id
-  where order_id=".$id." order by order.date";
+  where order_id=".$id." order by tracking.date";
   $data = getData($con,$query);
   if(count($data) > 0){
   $success="1";
