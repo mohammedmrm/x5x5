21517b8cd635f5ea9ab7c4dc83123f0136df6c2c
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-12 17:14:52 +0300

    auto update

--- charts/_getOrdersCount.php
+++ charts/_getOrdersCount.php
@@ -52,5 +52,24 @@ $sql = "SELECT
           GROUP BY from_branch";
 }
 $result = getData($con,$sql);
+ $sql = "select * from auto_update";
+ $res = getData($con,$sql);
+ foreach($res as $val){
+     ///-- auto status update ---
+     if($val['active'] == 1){
+     $auto = "SET @uids := '';
+               UPDATE
+               orders SET order_status_id = 4
+                WHERE order_status_id = 3 and invoice_id = 0 and driver_invoice_id = 0 and confirm=1 and city = '".$val['city_id']."'
+               DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids));
+               SELECT @uids as ids;";
+     $ids = getAllUpdatedIds($mysqlicon,$auto);
+     $ids = explode (",", $ids[0][0]);
+     $tracking = "insert into tracking (order_id,order_status_id,note,staff_id) values(?,?,?,?)";
+     foreach($ids as $id){
+       $addTrack = setData($con,$tracking,[$id,4,'( ?? ????? ????? ???????) ',$_SESSION['userid']]);
+     }
+   }
+ }
 echo json_encode(['data'=>$result]);
 ?>
