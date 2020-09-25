465c80a7e05e44b085a103bdfa8307a9238fd027
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-25 23:09:13 +0300

    new permission

--- script/_addClientAndPage.php
+++ script/_addClientAndPage.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(0);
 header('Content-Type: application/json');
 require("_access.php");
-access([1,2,3,5]);
+access([1,2,3,5,7]);
 require_once("dbconnection.php");
 require_once("_crpt.php");
 require("../config.php");
